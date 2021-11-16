ef299ba66957a036b45446e84157cb3a1c5bd49e
Aza Orda <ordaz.az@mail.ru>
2021-11-16 15:36:15 +0600

    input team number not less 1

---
+++ src/main/java/com/example/soccerrandomizer/repos/UserRepository.java
@@ -0,0 +1,9 @@
+/*package com.example.soccerrandomizer.repos;
+
+import com.example.soccerrandomizer.db.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+
+}
+*/
